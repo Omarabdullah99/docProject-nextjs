4b1ab0b2d90964f1d73580af66aac2f0d3b65d37
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-02-04 18:51:24 +0600

    sibar final logic setup

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,29 +1,72 @@
+"use client";
+import {
+  getDocumentsByAuthor,
+  getDocumentsByCategory,
+  getDocumentsByTag,
+} from "@/utils/doc-util";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Sidebar = ({ docs }) => {
   const allDocuments = docs;
-  console.log("alldocs", allDocuments);
+  const pathName = usePathname();
+  const [rootNodes, setRootNodes] = useState([]);
+  const [nonRootNodesGrouped, setNonRootNodesGrouped] = useState({});
 
-  // প্যারেন্ট ডকুমেন্টগুলো ফিল্টার করুন
-  const parentDocuments = allDocuments.filter((doc) => doc.parent === null);
+  useEffect(() => {
+    let matchedDocs = allDocuments;
 
-  // চাইল্ড ডকুমেন্টগুলো ফিল্টার করুন
-  const childDocuments = allDocuments.filter((doc) => doc.parent !== null);
+    if (pathName.includes("/tags")) {
+      const tag = pathName.split("/")[2];
+      //console.log("tag",tag)
+      matchedDocs = getDocumentsByTag(docs, tag);
+    } else if (pathName.includes("/author")) {
+      const author = pathName.split("/")[2];
+      //console.log('author',author)
+      matchedDocs = getDocumentsByAuthor(docs, author);
+    } else if (pathName.includes("/categories")) {
+      const category = pathName.split("/")[2];
+      matchedDocs = getDocumentsByCategory(docs, category);
+      //console.log('category',category)
+    }
 
-  const roots = allDocuments.filter((doc) => !doc.parent);
-  console.log("roots", roots);
+    const roots = matchedDocs.filter((doc) => !doc.parent);
 
-  const nonRoot = Object.groupBy(
-    allDocuments.filter((doc) => doc.parent),
-    ({ parent }) => parent
-  );
-  console.log("nonRoot", nonRoot);
+    const nonRoot = Object.groupBy(
+      matchedDocs.filter((doc) => doc.parent),
+      ({ parent }) => parent
+    );
+
+    const nonRootsKeys = Reflect.ownKeys(nonRoot);
+        nonRootsKeys.forEach(key => {
+            const foundInRoots = roots.find((root) => root.id === key);
+            if(!foundInRoots) {
+                const foundInDocs = docs.find((doc) => doc.id === key);
+                roots.push(foundInDocs);
+            }
+        });
+
+        roots.sort((a, b) => {
+            if (a.order < b.order) {
+              return -1;
+            }
+            if (a.order > b.order) {
+              return 1;
+            }
+            return 0;
+          });
+
+    console.log('matchDocs',matchedDocs)
+
+    setRootNodes([...roots]);
+    setNonRootNodesGrouped({ ...nonRoot });
+  }, [pathName]);
 
   return (
     <nav className="hidden lg:mt-10 lg:block">
       <ul role="list" className="border-l border-transparent">
-        {roots.map((rootNode) => (
+        {rootNodes.map((rootNode) => (
           <li key={rootNode.id} className="relative">
             <Link
               aria-current="page"
@@ -33,9 +76,9 @@ const Sidebar = ({ docs }) => {
               <span className="truncate">{rootNode.title}</span>
             </Link>
             {/* চাইল্ড ডকুমেন্টগুলো রেন্ডার করুন */}
-            {nonRoot[rootNode.id] && (
+            {nonRootNodesGrouped[rootNode.id] && (
               <ul role="list" className="border-l border-transparent">
-                {nonRoot[rootNode.id].map((subRoot) => (
+                {nonRootNodesGrouped[rootNode.id].map((subRoot) => (
                   <li key={subRoot.id}>
                     <Link
                       className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
