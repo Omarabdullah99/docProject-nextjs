616fae860d1357121de7a12da6da1f141197d022
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-02-04 14:59:38 +0600

    sidbar logic change and video logic apply: module7:11 e 1hr:2m:43s

--- components/ContentDisplay.jsx
+++ components/ContentDisplay.jsx
@@ -5,7 +5,7 @@ import Tag from './Tag';
 
 const ContentDisplay =async ({id}) => {
     const documentContent = await getDocumentContent(id);
-    console.log('docuemntContent',documentContent)
+    // console.log('docuemntContent',documentContent)
   return (
     <article className="prose dark:prose-invert">
       <h1>{documentContent.title}</h1>
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,42 +1,52 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Sidebar = ({ docs }) => {
   const allDocuments = docs;
+  console.log("alldocs", allDocuments);
 
   // প্যারেন্ট ডকুমেন্টগুলো ফিল্টার করুন
-  const parentDocuments = allDocuments.filter(doc => doc.parent === null);
+  const parentDocuments = allDocuments.filter((doc) => doc.parent === null);
 
   // চাইল্ড ডকুমেন্টগুলো ফিল্টার করুন
-  const childDocuments = allDocuments.filter(doc => doc.parent !== null);
+  const childDocuments = allDocuments.filter((doc) => doc.parent !== null);
+
+  const roots = allDocuments.filter((doc) => !doc.parent);
+  console.log("roots", roots);
+
+  const nonRoot = Object.groupBy(
+    allDocuments.filter((doc) => doc.parent),
+    ({ parent }) => parent
+  );
+  console.log("nonRoot", nonRoot);
 
   return (
     <nav className="hidden lg:mt-10 lg:block">
       <ul role="list" className="border-l border-transparent">
-        {parentDocuments.map(parentDoc => (
-          <li key={parentDoc.id} className="relative">
+        {roots.map((rootNode) => (
+          <li key={rootNode.id} className="relative">
             <Link
               aria-current="page"
               className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
-              href={`/docs/${parentDoc.id}`}
+              href={`/docs/${rootNode.id}`}
             >
-              <span className="truncate">{parentDoc.title}</span>
+              <span className="truncate">{rootNode.title}</span>
             </Link>
             {/* চাইল্ড ডকুমেন্টগুলো রেন্ডার করুন */}
-            <ul role="list" style={{ opacity: 1 }}>
-              {childDocuments
-                .filter(childDoc => childDoc.parent === parentDoc.id)
-                .map(childDoc => (
-                  <li key={childDoc.id}>
+            {nonRoot[rootNode.id] && (
+              <ul role="list" className="border-l border-transparent">
+                {nonRoot[rootNode.id].map((subRoot) => (
+                  <li key={subRoot.id}>
                     <Link
                       className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-                      href={`/docs/${parentDoc.id}/${childDoc.id}`}
+                      href={`/docs/${rootNode.id}/${subRoot.id}`}
                     >
-                      <span className="truncate">{childDoc.title}</span>
+                      <span className="truncate">{subRoot.title}</span>
                     </Link>
                   </li>
                 ))}
-            </ul>
+              </ul>
+            )}
           </li>
         ))}
       </ul>
